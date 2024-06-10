4aa6d90161c33938a700d37af40ffdd55c3b6efc
frxx222 <jayveeenobay040@gmail.com>
2024-06-10 13:05:13 +0800

    Update recipe.js

--- functions/schema/recipe.js
+++ functions/schema/recipe.js
@@ -1,26 +1,19 @@
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
+const { Schema } =  mongoose;
 
-const recipeSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    cuisine: {
-        type: String,
-        required: true,
-    },
-    ingredients: {
-        type: String,
-        required: true,
-    },
-    instructions: {
-        type: String,
-        required: true,
-    }
-
+const recipeSchema = new Schema ({
+name: {
+    type: String,
+    required: true,
+},
+ingredients: {
+    type: String,
+    required: true,
+},
+cuisine: {
+    type: String,
+    required: true,
+},
 });
-
-
 
 module.exports = recipeSchema;
