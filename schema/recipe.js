c835f0a8ff05f2d88d714d91e76d1b87d8487fd9
frxx222 <jayveeenobay040@gmail.com>
2024-06-10 13:03:17 +0800

    Add files via upload

---
+++ schema/recipe.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const recipeSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    cuisine: {
+        type: String,
+        required: true,
+    },
+    ingredients: {
+        type: String,
+        required: true,
+    },
+    instructions: {
+        type: String,
+        required: true,
+    }
+
+});
+
+
+
+module.exports = recipeSchema;
