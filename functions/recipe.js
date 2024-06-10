6eab28beba2d648455aeab38793d44e053a7ac3a
frxx222 <jayveeenobay040@gmail.com>
2024-06-10 12:58:25 +0800

    Update and rename functions/schema/author.js to functions/recipe.js

---
+++ functions/recipe.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const { Schema } =  mongoose;
+
+const recipeSchema = new Schema ({
+name: {
+    type: String,
+    required: true,
+},
+ingredients: {
+    type: String,
+    required: true,
+},
+cuisine: {
+    type: String,
+    required: true,
+},
+});
+
+module.exports = recipeSchema;
